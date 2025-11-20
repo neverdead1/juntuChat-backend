f32f289937963145ae402e7d2907be8d2432e90b
neverdead1 <55112480+neverdead1@users.noreply.github.com>
2025-11-20 10:22:29 -0400

    feat: conexion con la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
 import { ChatGateway } from './chat/chat.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ChatModule],
+  imports: [
+    ChatModule,
+    MongooseModule.forRoot('mongodb+srv://jhoanmijael_db_user:mYFYupBeXAeujodZ@chatcolaborativo.m8cvux3.mongodb.net/?appName=chatColaborativo'),
+  ],
   controllers: [],
   providers: [], //"ChatGateway" exportamos el getway creado de chat.gateway
 })
